0c0d53e22eb7634f569f7d06909ba821f3ce5caa
XiaoMing1992 <1422297148@qq.com>
2018-03-08 10:03:17 +0800

    增加二维码生成类
    增加二维码生成类

---
+++ MyAppDemo/app/src/main/java/guyuanjun/com/myappdemo/utils/QRCodeHelper.java
@@ -0,0 +1,57 @@
+package guyuanjun.com.myappdemo.utils;
+
+import android.graphics.Bitmap;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.util.Hashtable;
+
+
+public class QRCodeHelper {
+
+    /**
+     * 生成二维码
+     *
+     * @param text 需要生成二维码的文字、网址等
+     * @param size 需要生成二维码的大小
+     * @return bitmap
+     */
+    public static Bitmap createQRCode(String text, int size) {
+        if (size <= 0) {
+            return null;
+        }
+        try {
+            Hashtable<EncodeHintType, Object> hints = new Hashtable<>();
+            hints.put(EncodeHintType.CHARACTER_SET, "utf-8");
+            hints.put(EncodeHintType.MARGIN, 0);//背景留白间隙
+
+            BitMatrix bitMatrix = new QRCodeWriter().encode(text,
+                    BarcodeFormat.QR_CODE, size, size, hints);
+            int[] pixels = new int[size * size];
+            for (int y = 0; y < size; y++) {
+                for (int x = 0; x < size; x++) {
+                    if (bitMatrix.get(x, y)) {
+                        pixels[y * size + x] = 0xff000000;
+                    } else {
+                        pixels[y * size + x] = 0x00ffffff;
+                    }
+
+                }
+            }
+            Bitmap bitmap = Bitmap.createBitmap(size, size,
+                    Bitmap.Config.ARGB_8888);
+            bitmap.setPixels(pixels, 0, size, 0, 0, size, size);
+            return bitmap;
+        } catch (WriterException e) {
+            e.printStackTrace();
+            return null;
+        } catch (Error e) {
+            return null;
+
+        }
+    }
+}
---
+++ MyChat/app/src/main/java/guyuanjun/com/mychat/adapter/MyAdapter.java
@@ -0,0 +1,77 @@
+package guyuanjun.com.mychat.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+import java.util.Map;
+
+import guyuanjun.com.mychat.R;
+
+/**
+ * Created by HP on 2018-3-1.
+ */
+
+public class MyAdapter extends BaseAdapter {
+    private LayoutInflater mLayoutInflater;
+    private Context mContext;
+    private List<Map<String, ?>> mData;
+
+    public MyAdapter(Context context, final List<Map<String, ?>> data) {
+        mContext = context;
+        mData = data;
+        mLayoutInflater = LayoutInflater.from(mContext);
+    }
+
+    @Override
+    public int getCount() {
+        return mData == null ? 0 : mData.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mData == null ? null : mData.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Holder holder;
+        if (convertView == null) {
+            convertView = mLayoutInflater.inflate(R.layout.item, parent, false);
+            holder = new Holder();
+            convertView.setTag(holder);
+        } else {
+            holder = (Holder) convertView.getTag();
+
+        }
+        String content = (String) mData.get(position).get("content");
+        int id = ((Integer) mData.get(position).get("id")).intValue();
+        if (id == 0) {
+            holder.left_tv = (TextView) convertView.findViewById(R.id.left_tv);
+            holder.left_tv.setVisibility(View.VISIBLE);
+            if (holder.right_tv != null) holder.right_tv.setVisibility(View.GONE);
+
+            holder.left_tv.setText(content);
+        } else if (id == 1) {
+            holder.right_tv = (TextView) convertView.findViewById(R.id.right_tv);
+            holder.right_tv.setVisibility(View.VISIBLE);
+            if (holder.left_tv != null) holder.left_tv.setVisibility(View.GONE);
+            holder.right_tv.setText(content);
+        }
+        return convertView;
+    }
+
+    class Holder {
+        TextView left_tv;
+        TextView right_tv;
+    }
+}
