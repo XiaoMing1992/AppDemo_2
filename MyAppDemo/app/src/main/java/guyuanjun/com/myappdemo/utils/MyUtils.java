32e7bad67e76ebcacb2b93d701a39229a86a90fa
XiaoMing1992 <1422297148@qq.com>
2018-07-04 15:40:17 +0800

    上传判断字符串长度的方法
    包含中英文的字符串的长度的判断方法

--- MyAppDemo/app/src/main/java/guyuanjun/com/myappdemo/utils/MyUtils.java
+++ MyAppDemo/app/src/main/java/guyuanjun/com/myappdemo/utils/MyUtils.java
@@ -226,4 +226,57 @@ public class MyUtils {
         return context.getCacheDir().getAbsolutePath();
     }
 
+    /**
+     * 计算包含中文的字符串的长度
+     * @param value
+     * @return
+     */
+    public static int length(String value) {
+        int valueLength = 0;
+        String chinese = "[\u0391-\uFFE5]";
+        /* 获取字段值的长度，如果含中文字符，则每个中文字符长度为2，否则为1 */
+        for (int i = 0; value != null && i < value.length(); i++) {
+            /* 获取一个字符 */
+            String temp = value.substring(i, i + 1);
+            /* 判断是否为中文字符 */
+            if (temp.matches(chinese)) {
+                /* 中文字符长度为2 */
+                valueLength += 2;
+            } else {
+                /* 其他字符长度为1 */
+                valueLength += 1;
+            }
+        }
+        return valueLength;
+    }
+
+    /**
+     * 计算包含中文的字符串的长度
+     * @param value
+     * @return
+     */
+    public static String getStrByLength(String value, int len) {
+        int valueLength = 0;
+        String str = "";
+        String chinese = "[\u0391-\uFFE5]";
+        /* 获取字段值的长度，如果含中文字符，则每个中文字符长度为2，否则为1 */
+        for (int i = 0; value != null && i < value.length(); i++) {
+            /* 获取一个字符 */
+            String temp = value.substring(i, i + 1);
+            /* 判断是否为中文字符 */
+            if (temp.matches(chinese)) {
+                /* 中文字符长度为2 */
+                valueLength += 2;
+            } else {
+                /* 其他字符长度为1 */
+                valueLength += 1;
+            }
+            str += temp;
+            if (len == valueLength){
+
+                break;
+            }
+        }
+        return str;
+    }
 }
